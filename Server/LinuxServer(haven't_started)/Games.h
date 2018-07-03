f0143fb8027ddd06c1f8863e2ad4ddcf7ac2db86
Jiachenggavin <ghsuxjc@gmail.com>
2018-07-02 20:54:53 -0700

    copy the windows server files and these files will be edited to a linux version in the future.

---
+++ Server/LinuxServer(haven't_started)/Games.h
@@ -0,0 +1,87 @@
+﻿#pragma once
+
+#include <string>
+#include <iostream>
+#include <memory>
+
+
+class Player;
+typedef std::shared_ptr<Player> player;
+
+class Game
+{
+public:
+	friend class Server;
+	Game(int id, int host_id, std::string host_name)
+	{
+		this->id = id;
+		this->host_id = host_id;
+		this->hostName = host_name;
+	}
+	void Joinin(int joinID, player Join_player, std::string p2Name)
+	{
+		this->guest_id = joinID;
+		player2 = Join_player;
+		this->p2Name = p2Name;
+		isplay = 1;
+	}
+	void playAgain(int playID)
+	{
+		if (playID == host_id)
+			host_play_again = true;
+		else
+			player2_play_again = true;
+	}
+	bool can_Play_again()
+	{
+		return (host_play_again && player2_play_again);
+	}
+	void reset_play_again()
+	{
+		host_play_again = false;
+		player2_play_again = false;
+	}
+	void hostIs(player host)
+	{
+		this->host = host;
+	}
+	bool isFull()
+	{
+		return (guest_id != -1);
+	}
+	int hostsID()
+	{
+		return host_id;
+	}
+	int anotherPlayerID(int onePlayerID)
+	{
+		if (onePlayerID == host_id)
+			return guest_id;
+		else if (onePlayerID == guest_id)
+			return host_id;
+		else
+			return -1;
+	}
+	void booltest()
+	{
+		if (host_play_again)
+			std::cout << "host_play_again is TRUE" << std::endl;
+		else
+			std::cout << "host_play_again is FALSE" << std::endl;
+		if (player2_play_again)
+			std::cout << "player2_play_again is TRUE" << std::endl;
+		else
+			std::cout << "player2_play_again is FALSE" << std::endl;
+	}
+private:
+	int id;
+	int host_id;
+	std::string hostName;
+	std::string p2Name;
+	int guest_id = -1;
+	int isplay = 0;
+	bool host_play_again = false;
+	bool player2_play_again = false;
+	player host = NULL;
+	player player2 = NULL;
+};
---
+++ Server/LinuxServer(haven't_started)/Server.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib,"ws2_32.lib")
+#include <WinSock2.h>
+#include <string>
+#include <memory>
+#include <iostream>
+#include <process.h>
+#include "Games.h"
+#include "Player.h"
+#include <unordered_map>
+#include "cJSON\cJSON.h"
+
+//typedef std::vector<std::vector<unique_ptr>> BOARD;
+
+using namespace std;
+
+typedef shared_ptr<Game> onlineGame;
+typedef shared_ptr<Player> player;
+
+
+class Server
+{
+public:
+	Server(int PORT, bool BroadcastPublically = false);
+	bool ListenForNewConnection();
+
+private:
+	bool SendString(int ID, string & _string);
+	bool GetString(int ID, string & _string);
+	void sendMessToClients(string Message);
+	bool Processinfo(int ID);
+	bool CreateGameList(string & _string);
+	bool sendSystemInfo(int ID, string InfoType);
+	bool sendGameList(int ID); // if ID <0 ,means send gamelist to all clients
+	void deleteGame(int ID);
+
+	static void ClientHandlerThread(void* IDptr);
+
+private:
+	//-------------------------------------------
+	// online variables:
+	//-------------------------------------------
+	unordered_map<int, SOCKET> Connections;
+	//SOCKET Connections[512];
+	int TotalConnections = 0;
+	int allID = 0;
+	SOCKADDR_IN addr;                //Address that we will bind our listening socket to
+	int addrlen = sizeof(addr);
+	SOCKET sListen;
+	//---------------------------------------
+	//Logic variables:
+	//----------------------------------------
+	int GameNum = 0;
+	unordered_map<int, onlineGame> GameList;
+	unordered_map<int, player> PlayerList;
+};
+
+static Server * serverptr; 
